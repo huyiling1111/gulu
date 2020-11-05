026b51b4ed40fbb42ac3ab18f67424e0ebb4e808
huyiling1111 <1052859577@qq.com>
2020-11-05 16:55:51 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div></div>
+<Zero />
+</template>
+
+<script>
+import Zero from './components/Zero.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Zero
+    }
+}
+</script>
