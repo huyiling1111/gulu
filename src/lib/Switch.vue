1ddd40a470f2d4ed122a58ce3ec397bab10a402e
huyiling1111 <1052859577@qq.com>
2020-11-07 00:15:09 +0800

    去除点击按钮时显示的黑色边框

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -41,6 +41,7 @@ span {
     width: $h2;
     background: white;
     border-radius: $h2 / 2;
+    transition: left 250ms;
 }
 
 button.checked {
@@ -49,5 +50,9 @@ button.checked {
 
 button.checked>span {
     left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+    outline: none;
 }
 </style>
