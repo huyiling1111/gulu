50c067f70b294512c7d496face29aa39e096def9
huyiling1111 <1052859577@qq.com>
2020-11-07 23:31:20 +0800

    支持size属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,17 @@
 <template>
 <div>Button 示例</div>
 <h1>示例1</h1>
-
-<Button theme="button">你好</Button>
-<Button theme="link">你好</Button>
-<Button theme="text">你好</Button>
+<div>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+</div>
+<h1>示例2</h1>
+<div>
+    <Button size="big">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
+</div>
 </template>
 
 <script lang="ts">
