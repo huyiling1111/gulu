4b68255b5c8c5baf2091a198c2668260be198325
huyiling1111 <1052859577@qq.com>
2020-11-09 10:46:57 +0800

    引入github样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -92,7 +92,7 @@ export default {
 aside {
     background: rgb(229, 240, 178);
     width: 150px;
-    padding: 16px;
+    padding: 16px 0;
     position: fixed;
     top: 0;
     left: 0;
@@ -101,11 +101,20 @@ aside {
 
     >h2 {
         margin-bottom: 4px;
+        padding: 0 16px;
     }
 
     >ol {
         >li {
-            padding: 4px 0;
+            >a {
+                display: block;
+                padding: 4px 16px;
+                text-decoration: none;
+            }
+
+            .router-link-active {
+                background: white;
+            }
         }
     }
 }
