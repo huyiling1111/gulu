fbf6626d67cf16da5e8a02bc6e87ee35894ea2e0
huyiling1111 <1052859577@qq.com>
2020-11-11 11:53:35 +0800

    添加rollup.config.js

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
-git branch -M master &&
+git branch -M main &&
 git remote add origin git@github.com:huyiling1111/gulu-ui-website.git &&
-git push -f -u origin master &&
+git push -f -u origin main &&
 cd .. &&
 ehco https://huyiling1111.github.io/gulu-ui-website/index.html
